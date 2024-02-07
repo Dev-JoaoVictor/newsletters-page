f601d7ccbe2b0c5eac2c1445e88993d0cd0e5ae0
João <joaovictor.ramos34@gmail.com>
2024-02-07 14:57:39 -0300

    feat(inicio): adiciona as cores do tema e bibliotecas para desenvolvimento

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '375px',
+      },
+      colors: {
+        GREY_300: 'hsl(234, 29%, 20%)',
+        GREY_200: 'hsl(235, 18%, 26%)',
+        GREY_100: 'hsl(231, 7%, 60%)',
+        TOMATO: 'hsl(4, 100%, 67%)',
+      }
+    },
+  },
+  plugins: [],
+}
